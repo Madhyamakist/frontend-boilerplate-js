6661a0fac9677ed5c7f67764d586c9fe7a67fad4
Prachi Nayak <prachinayak1509@gmail.com>
2025-06-30 12:58:26 +0530

    Setting up project using next.js (#2)
    * README and scripts
    
    * deleted scripts and updated README
    
    * updated readme
    
    * fixed readme
    
    * initialized Next.js project
    
    * remove unnecessary next.js stuff

---
+++ code/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className=" items-center justify-items-center ${montserrat.variable}">
+      <h1>Hello World
+      </h1>
+    </div>
+  );
+}
