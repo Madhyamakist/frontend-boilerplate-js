c22d4ac678f2e790b556a8cb265613a287350c69
prachinayak1509 <prachinayak1509@gmail.com>
2025-07-03 15:50:19 +0530

    demo header and footer

--- code/src/app/page.tsx
+++ code/src/app/page.tsx
@@ -2,13 +2,21 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World
-      </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      </p>
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <span className="head-text">Madhyamakist</span>
+      </header>
+      <main className="flex-1 p-4">
+        <h1>Hello World
+        </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+      </main>
+      <footer>
+        <span> © 2025 SmallTech Madhyamakist. All rights reserved.</span>
+      </footer>
     </div>
   );
 }
