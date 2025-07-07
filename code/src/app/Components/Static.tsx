91044b555484f688eb9b6de31df20f1380ee1e17
prachinayak1509 <prachinayak1509@gmail.com>
2025-07-07 16:06:00 +0530

    static page

--- code/src/app/Components/Static.tsx
+++ code/src/app/Components/Static.tsx
@@ -1,12 +1,20 @@
+import Section from '../Components/Section';
+import Image from 'next/image';
+
 export default function Static() {
     return (
-        <div className="p-4">
+        <div className="p-4 justify-center">
             <h1>Hello World</h1>
-            <h2>Lorem ipsum</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            </p>
+            <Section />
+            <Section />
+            <Image
+                src="/demo.png"
+                alt="Demo Image"
+                width={320}
+                height={100}
+                className=' mx-auto my-4'
+            />
+            <Section />
         </div>
     )
 }
